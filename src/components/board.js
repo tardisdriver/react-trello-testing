17a8b6c2430a25e5f39c730fc18044dc37d5d10c
tardisdriver <stark.tracy@gmail.com>
2017-10-31 19:59:19 -0500

    first commit

---
+++ src/components/board.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+import List from './list';
+import AddForm from './add-form';
+
+import './board.css';
+
+export default class Board extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            lists: []
+        };
+
+        this.addList = this.addList.bind(this);
+    }
+
+    addList(title) {
+        this.setState({
+            lists: [...this.state.lists, {title}]
+        });
+    }
+
+    render() {
+        const lists = this.state.lists.map((list, index) =>
+            <List key={index} {...list} />
+        );
+
+        return (
+            <div className="board">
+                <h2>{this.props.title}</h2>
+                <div className="lists">
+                    {lists}
+                    <AddForm type="list" onAdd={this.addList} />
+                </div>
+            </div>
+        );
+    }
+}
+
+Board.defaultProps = {
+    title: 'Board'
+};
+
---
+++ src/components/list.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import Card from './card';
+import AddForm from './add-form';
+
+import './list.css';
+
+export default class List extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            cards: []
+        }
+
+        this.addCard = this.addCard.bind(this);
+    }
+
+    addCard(text) {
+        this.setState({
+            cards: [...this.state.cards, {
+                text
+            }]
+        });
+    }
+
+    render() {
+        const cards = this.state.cards.map((card, index) =>
+            <Card key={index} {...card} />
+        );
+        return (
+            <div className="list">
+                <h3>{this.props.title}</h3>
+                {cards}
+                <AddForm type="card" onAdd={this.addCard} />
+            </div>
+        );
+    }
+}
+
+List.defaultProps = {
+    title: ''
+};
